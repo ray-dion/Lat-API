390cb7ea6b65827426c3473ca5957608a81e8255
ray-dion <raydionisius@gmail.com>
2025-02-06 09:23:31 +0700

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import pandas as pd
+
+# create API object
+app = FastAPI()
+
+# read data
+data = pd.read_csv('data.csv')
+
+# coba root home API (get)
+@app.get("/")
+def root():
+    return {'message':'Hello HCK 025 !'}
+
+# endpoint sapaan
+@app.get("/name/{name}")
+def greet(name):
+    return {'message': f'Hai {name}, how are you ?'}
+
+# endpoint return data
+@app.get("/data")
+def get_data():
+    return data.to_dict(orient='records')
+
+# get data by id
+@app.get("/data/{id}")
+def search_data(id:int):
+    result = data[data['id']==id]
+    return {'result': result.to_dict(orient='records')}
+
+# # menambahkan data
+@app.post("/data/add")
+def add_data(new_data:dict):
+    global data
+    
+    new_row = pd.DataFrame([new_data])
+    data = pd.concat([data, new_row], ignore_index=True)
+
+    return {'message':data.to_dict(orient='records')}
